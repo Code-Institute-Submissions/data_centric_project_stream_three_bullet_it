b00d8c605d6a20d1231769744f25615ad2f7a607
Eva Larkin <larkineva@gmail.com>
2018-07-27 15:44:15 +0000

    basic login/out functionality with html templates

--- app.py
+++ app.py
@@ -1,13 +1,26 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from pymongo import MongoClient
+from bson.objectid import ObjectId
 
+
+MONGODB_URI = os.environ.get("MONGODB_URI")
+MONGODB_NAME = os.environ.get("MONGODB_NAME")
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hello World'
+def get_index():
+    return render_template('login.html')
     
-    
+@app.route("/login", methods=['POST'])
+def do_login():
+    username = request.form['username']
+    return redirect(username)
+
+@app.route("/<username>") 
+def get_userpage(username):
+    return render_template("index.html", username=username)
+
 if __name__ == '__main__':
     app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)), debug=True)
