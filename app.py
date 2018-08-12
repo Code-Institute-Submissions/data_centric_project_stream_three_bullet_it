46c739d300a92306d8b8473c737f0e021ba897aa
Eva Larkin <larkineva@gmail.com>
2018-08-12 17:40:15 +0000

    login/home page added & materialize styling added to all pages

--- app.py
+++ app.py
@@ -47,12 +47,17 @@ def delete_collection(username, collection_id):
 @app.route("/<username>/edit_collection/<collection_id>")
 def edit_collection(username, collection_id):
     collection=mongo.db[username].find_one({'_id': ObjectId(collection_id)})
-    return render_template('edit_collection.html', username=username, collection=collection)
+    return render_template('edit_collection.html', username=username, collection=collection, collection_id=collection_id)
 
 @app.route("/<username>/update_collection/<collection_id>", methods=['POST'])
 def update_collection(username, collection_id):
     mongo.db[username].update({"_id":ObjectId(collection_id)}, {"$set": {"name": request.form['collection_name']}})
     return redirect(username + "/get_collections")
+
+# @app.route("/<username>/edit_collection/<collection_id>/<item_id>")
+# def edit_collection(username, collection_id, item_id):
+#     collection=mongo.db[username].find_one({'_id': ObjectId(collection_id)})
+#     return render_template('edit_collection_item.html', username=username, collection=collection, collection_id=collection_id, item_id=item_id)
 
 @app.route("/<username>/update_collection/<collection_id>/<item_id>", methods=['POST'])
 def update_collection_item_name(username, collection_id, item_id):
@@ -62,18 +67,16 @@ def update_collection_item_name(username, collection_id, item_id):
     return redirect(username + "/get_collections")
   
   
-@app.route("/<username>/<collection_name>") 
-def view_collection_by_user(username, collection_name):
-    collection_items = load_collection_items_from_mongo(username, collection_name)
-    return render_template("collection_items.html", username=username, collection_name=collection_name, collection_items=collection_items)
+@app.route("/<username>/<collection_name>/<collection_id>") 
+def view_collection_by_user(username, collection_name, collection_id):
+    collection_items = load_collection_items_from_mongo(username, collection_id)
+    return render_template("collection_items.html", username=username, collection_name=collection_name, collection_items=collection_items, collection_id=collection_id)
 
     
-@app.route("/<username>/<collection_name>/add_item", methods=["POST"]) 
-def add_item_to_collection(username, collection_name):
-    print ("here")
-    print (collection_name)
+@app.route("/<username>/<collection_name>/<collection_id>/add_item", methods=["POST"]) 
+def add_item_to_collection(username, collection_name, collection_id):
     collection_item= request.form['collection_item_name']
-    max_id = get_max_id(username, collection_name)
+    max_id = get_max_id(username, collection_name, collection_id)
     try: 
         int(max_id)
         new_id = max_id
@@ -81,12 +84,12 @@ def add_item_to_collection(username, collection_name):
         new_id = 1
     
     collection_item_details = {"collection_item_name": collection_item, "item_id": new_id }
-    save_collection_items_to_mongo(username, collection_name, collection_item_details)
-    return redirect(username + "/" + collection_name)
+    save_collection_items_to_mongo(username, collection_name, collection_id, collection_item_details)
+    return redirect(username + "/" + collection_name + "/" + collection_id)
 
 #---------------------------------------functions--------------------------------------
-def get_max_id(username, collection_name):
-        collection = mongo.db[username].find_one({"name": collection_name})
+def get_max_id(username, collection_name, collection_id):
+        collection = mongo.db[username].find_one({"_id":ObjectId(collection_id)})
         # Here we want to loop through the collection and get the highest id already in use, then add One to it
         max_id = 0
         for each in collection['collection_items']:
@@ -102,13 +105,13 @@ def create_collection_for_user(username, collection_name):
 def load_collections_by_username(username):
         return mongo.db[username].find()
 
-def save_collection_items_to_mongo(username, collection_name, new_collection_item):
-        selected_collection = mongo.db[username].find_one({'name':collection_name})
+def save_collection_items_to_mongo(username, collection_name, collection_id, new_collection_item):
+        selected_collection = mongo.db[username].find_one({"_id":ObjectId(collection_id)})
         selected_collection['collection_items'].append(new_collection_item)
         mongo.db[username].save(selected_collection)
 
-def load_collection_items_from_mongo(username, collection_name):
-        return mongo.db[username].find({'name':collection_name})
+def load_collection_items_from_mongo(username, collection_id):
+        return mongo.db[username].find({"_id":ObjectId(collection_id)})
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'), port=int(os.environ.get('PORT')), debug=True)
